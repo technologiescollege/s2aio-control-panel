51311839317142899c512fa32339146339ade53e
SebCanet <sebastien.canet@ac-nantes.fr>
2016-01-18 23:45:24 +0100

    v2.1.7
    v2 for control panel : detect and control processus
    and v.7 for s2aio process

---
+++ scratch2.bat
@@ -0,0 +1,7 @@
+@echo off
+break ON
+rem fichiers BAT et fork créés par Sébastien CANET
+cls
+SET currentpath=%~dp1
+SET dossier_scratch="B:\LiberKey\MyApps\Scratch 2\Scratch 2.exe"
+start %dossier_scratch% "S:\Perso\GitHub\technologies\s2aio-control-panel\bibliotheque\s2aio_Scratch2Arduino.sb2"
