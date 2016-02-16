18c2f7728beb9cf17ffb4909f3a079e8012f63b8
SebCanet <sebastien.canet@ac-nantes.fr>
2016-02-16 15:43:49 +0100

    update control panel

--- s2aio_cmd.bat
+++ s2aio_cmd.bat
@@ -4,4 +4,4 @@ rem fichiers BAT et fork créés par Sébastien CANET
 SET currentpath=%~dp1
 cd %currentpath%s2aio
 cls
-python.exe .\Lib\site-packages\s2aio\__main__.py -l 5 -c no_client -p COM7
+python.exe .\Lib\site-packages\s2aio\__main__.py -l 5 -c no_client -p COM1
