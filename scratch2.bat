95d8031f3a3a341c3b6dc9aa6fef6a11b6c8bb92
SebCanet <sebastien.canet@ac-nantes.fr>
2016-09-28 23:23:46 +0200

    embed icons in exe file

--- scratch2.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-break ON
-rem fichiers BAT et fork créés par Sébastien CANET
-cls
-SET currentpath=%~dp1
-SET dossier_scratch="C:\Logiciels\MyApps\Scratch 2\Scratch 2.exe"
-start %dossier_scratch% "F:\Logiciels\Arduino_graphique\s2aio\bibliotheque\s2aio_Scratch2Arduino.sb2"
