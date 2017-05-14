b2ac255d7ae877e230b08c4ceca88d08518a92a0
SebCanet <scanet@libreduc.cc>
2017-05-14 19:57:37 +0200

    update 2.1.10

---
+++ s2aio_cmd.bat
@@ -0,0 +1,7 @@
+@echo off
+break ON
+rem fichiers BAT et fork créés par Sébastien CANET
+SET currentpath=%~dp1
+cd %currentpath%s2aio
+cls
+python.exe .\Lib\site-packages\s2aio\__main__.py -l 5 -c no_client -p COM4
